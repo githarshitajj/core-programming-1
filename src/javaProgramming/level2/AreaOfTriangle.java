e8dd18859293629c0518329100265ccf582cbbf0
githarshitajj <harshitajangra1703@gmail.com>
2025-02-20 09:59:57 +0530

    completed javaProgramming level2

---
+++ src/javaProgramming/level2/AreaOfTriangle.java
@@ -0,0 +1,9 @@
+package javaProgramming.level2;
+
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        javaProgramming.leve1.AreaOfTriangle.main(new String[0]);
+    }
+}
